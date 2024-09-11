33b10c6a639502b3652a3a6c1f25398caca16542
Musa Yazlık <musayazlik1997@gmail.com>
2024-09-11 14:28:39 +0300

    Refactor code to adhere to Tamagui component library and update dependencies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,11 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+import { Avatar, XStack } from "tamagui";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -11,24 +13,42 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={color}
+            />
+          ),
+
+          headerLeft: () => (
+            <XStack alignItems="center" gap="$6" padding="$3">
+              <Avatar circular size="$3">
+                <Avatar.Image
+                  accessibilityLabel="Cam"
+                  src="https://images.unsplash.com/photo-1548142813-c348350df52b"
+                />
+                <Avatar.Fallback backgroundColor="$blue10" />
+              </Avatar>
+            </XStack>
           ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: "Explore",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "code-slash" : "code-slash-outline"}
+              color={color}
+            />
           ),
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,83 +1,194 @@
+import React, { useEffect, useState } from "react";
 import {
-  Image,
-  StyleSheet,
-  Platform,
-  TextInput,
-  Text,
   View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
 } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { db } from "@firebase";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  limit,
+  startAfter,
+  DocumentData,
+} from "@firebase/firestore";
+import { Button, Card, H2, Paragraph, XStack } from "tamagui";
+import { Post } from "@/types/post";
+import { Plus } from "@tamagui/lucide-icons";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [lastDoc, setLastDoc] = useState<DocumentData | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [loadingMore, setLoadingMore] = useState<boolean>(false);
+  const POSTS_LIMIT = 5;
+  const fetchData = async (isInitialLoad = false) => {
+    try {
+      setLoading(true);
+      const postQuery = isInitialLoad
+        ? query(
+            collection(db, "posts"),
+            orderBy("date", "desc"),
+            limit(POSTS_LIMIT)
+          )
+        : query(
+            collection(db, "posts"),
+            orderBy("date", "desc"),
+            startAfter(lastDoc),
+            limit(POSTS_LIMIT)
+          );
+
+      const querySnapshot = await getDocs(postQuery);
+      const data = querySnapshot.docs.map((doc: DocumentData) => ({
+        ...doc.data(),
+        date: doc.data().date.toDate(),
+      })) as Post[];
+      setPosts((prevPosts) => (isInitialLoad ? data : [...prevPosts, ...data]));
+      setLastDoc(querySnapshot.docs[querySnapshot.docs.length - 1]);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    } finally {
+      setLoading(false);
+      setLoadingMore(false);
+    }
+  };
+
+  const fetchMoreData = () => {
+    if (!loadingMore && lastDoc) {
+      setLoadingMore(true);
+      fetchData();
+    }
+  };
+
+  const createPost = async () => {
+    try {
+      await addDoc(collection(db, "posts"), {
+        title: "Yeni Post",
+        description: "Bu bir açıklamadır.",
+        date: new Date(),
+        image_url: "https://images.unsplash.com/photo-1548142813-c348350df52b",
+      });
+      fetchData(true);
+    } catch (error) {
+      console.error("Error adding post:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(true);
+  }, []);
+
+  const renderItem = ({ item }: { item: Post }) => (
+    <Card
+      elevate
+      size="$4"
+      bordered
+      backgroundColor={"$white05"}
+      style={styles.card}
     >
-      <ThemedView style={styles.titleContainer}>
-        <View>
-          <Text style={{ color: "white" }}>Hello</Text>
-          <TextInput style={{ color: "white" }} />
-        </View>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <Card.Header padded>
+        <H2>{item.title}</H2>
+        <Paragraph theme="alt2">
+          {new Date(item.date).toLocaleDateString()}
+        </Paragraph>
+      </Card.Header>
+      <View style={styles.cardContent}>
+        <Image source={{ uri: item.image_url }} style={styles.image} />
+        <Text style={styles.description}>{item.description}</Text>
+      </View>
+      <Card.Footer padded>
+        <XStack justifyContent="flex-end">
+          <Button borderRadius="$10" theme="blue">
+            Read More
+          </Button>
+        </XStack>
+      </Card.Footer>
+    </Card>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Home Screen</Text>
+        <Button
+          variant="outlined"
+          icon={<Plus size={20} />}
+          onPress={createPost}
+          theme={"blue_alt1"}
+          forceStyle="hover"
+        >
+          New Post
+        </Button>
+      </View>
+      <FlatList
+        data={posts}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={styles.list}
+        onEndReached={fetchMoreData}
+        onEndReachedThreshold={0.1}
+      />
+      {loading && <ActivityIndicator size="small" color="#0000ff" />}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f0f0",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  headerContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
     flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    gap: 8,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e0e0e0",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  list: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  cardContent: {
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f0f0f0",
+    marginBottom: 10,
+  },
+  description: {
+    marginTop: 10,
+    color: "#666",
+    fontSize: 14,
+  },
+  image: {
+    width: "100%",
+    height: 180,
+    borderRadius: 10,
+    marginVertical: 10,
   },
 });
