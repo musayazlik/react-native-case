96573e05571a8d5ef3ee7bdde4907280cad2b6dd
Musa Yazlık <musayazlik1997@gmail.com>
2024-09-16 22:59:30 +0300

    Add formatDateFromTimestamp helper function

---
+++ helpers/formatDateFromTimestamp.ts
@@ -0,0 +1,12 @@
+function formatDateFromTimestamp(timestamp: {
+  seconds: number;
+  nanoseconds: number;
+}): string {
+  const date = new Date(timestamp.seconds * 1000);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
+export default formatDateFromTimestamp;
